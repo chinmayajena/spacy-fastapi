c5f9467bd2ab7602bc5a8ed1cff6adc12a4fc0ca
chinmay <mail2chinmaya.jena@gmail.com>
2023-04-01 22:03:37 +0530

    added end points with large model for english on spacy

--- Readme.md
+++ Readme.md
@@ -5,5 +5,15 @@ python -m pip install fastapi uvicorn[standard]
 ```
 
 ```
+pip install spacy
+
+python -m spacy download en_core_web_lg
+```
+
+```
 uvicorn main:app --reload
 ```
+
+Visit:
+
+http://localhost:8000/docs
--- main.py
+++ main.py
@@ -1,7 +1,35 @@
 from fastapi import FastAPI
+import spacy
+from pydantic import BaseModel
 
-app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+en_core_web = spacy.load("en_core_web_lg")
+app = FastAPI(tags=['sentence'])
+
+class Input(BaseModel):
+    sentence: str
+
+@app.post("/analyze_text")
+def get_text_characteristics(sentence_input: Input):
+    document = en_core_web(sentence_input.sentence)
+    output_array = []
+    for token in document:
+        output = {
+            "Index": token.i, "Token": token.text, "Tag": token.tag_, "POS": token.pos_,
+            "Dependency": token.dep_, "Lemma": token.lemma_, "Shape": token.shape_,
+            "Alpha": token.is_alpha, "Is Stop Word": token.is_stop
+        }
+        output_array.append(output)
+    return {"output": output_array}
+
+@app.post("/entity_recognition")
+def get_entity(sentence_input: Input):
+    document = en_core_web(sentence_input.sentence)
+    output_array = []
+    for token in document.ents:
+        output = {
+            "Text": token.text, "Start Char": token.start_char,
+            "End Char": token.end_char, "Label": token.label_
+        }
+        output_array.append(output)
+    return {"output": output_array}
